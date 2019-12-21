0c7b022fa3c7a6c37383a6de126321d56b7b2fb9
rhayem <etudiant.abderrahim.jridi@uvt.tn>
2019-12-21 21:43:58 +0100

    add data service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { from } from 'rxjs';
+import { DataService } from '../data.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,16 +12,19 @@ itemCount: number = 4;
 btnText: string = 'Add an Item';
 goalText: string = 'My first life goal';
 goals =[];
-  constructor() { }
+  constructor( private dataService:DataService,private router: Router ) {
+    this.goals=this.dataService.getGoal();
+   }
 
   ngOnInit() {
 this.itemCount = this.goals.length;
   }
 
 addItem(){
-this.goals.push(this.goalText);
+//this.goals.push(this.goalText);
+this.dataService.goals.push(this.goalText);
 this.goalText='';
-this.itemCount = this.goals.length;
-
+//this.itemCount = this.goals.length;
+this.itemCount = this.dataService.goals.length;
 }
 }
